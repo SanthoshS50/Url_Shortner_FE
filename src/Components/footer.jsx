426a9c3ec65b9f7f58151f5321a5264e228df5f7
Santhosh Srinivasan <santhoshsrinivasan29092001@gmail.com>
2024-02-19 12:44:00 +0530

    Task Updated

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -10,7 +10,7 @@ const Footer = () =>{
   <div className="text-center p-4" style={{backgroundColor: "rgba(0, 0, 0, 0.05)"}}>
     © 2024 Copyright : 
     <br/>
-    <a className="text-reset fw-bold" href="https://github.com/deepakrawat0007/URL-Shortner">Santhosh Srinivasan (URL Shortner)</a>
+    <a className="text-reset fw-bold" href="https://url-shortner-fe-blush.vercel.app">Santhosh Srinivasan (URL Shortner)</a>
   </div>
   {/* <!-- Copyright -->*/}
 </footer>
